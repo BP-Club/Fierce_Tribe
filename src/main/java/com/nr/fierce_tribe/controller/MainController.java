0e75f52d8fba375b7da3bad9e1f8847947386bf6
zh113259 <zhuhao_seu@163.com>
2020-01-09 10:26:24 +0800

    zh

--- src/main/java/com/nr/fierce_tribe/controller/MainController.java
+++ src/main/java/com/nr/fierce_tribe/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.nr.fierce_tribe.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class MainController {
 
-
+    @RequestMapping("/test")
+    public JSONObject test(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("data","HelloWorld");
+        jsonObject.put("resultCode", "200");
+        jsonObject.put("success", true);
+        jsonObject.put("message","操作成功");
+        return jsonObject;
+    }
 
 }
